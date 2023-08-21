3b0e9819a4fb38d72af400623eec598bfc56d5d2
Evgeny Namsaraev <enamsaraev13@icloud.com>
2023-08-21 23:17:56 +0300

    switch db

--- hstest/settings.py
+++ hstest/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'hstest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-switch_db = 1
+switch_db = 0
 if switch_db:
     DATABASES = {
         'default': {
